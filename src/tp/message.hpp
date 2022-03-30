09e14fde020665ae52260d970ea310632a6487bc
lichen-liu <issac.liu@mail.utoronto.ca>
2022-03-30 19:45:46 -0400

    add warn message

--- src/tp/message.hpp
+++ src/tp/message.hpp
@@ -1,16 +1,22 @@
 #pragma once
 #include <cstdio>
 
-#define MESSAGE_LEVEL 0
+#define MESSAGE_LEVEL 3
 
-#if MESSAGE_LEVEL >= 0
-#define info(fmt, ...) printf("I-" fmt, __VA_ARGS__)
+#if MESSAGE_LEVEL >= 3
+#define warn(fmt, ...) printf("W-" fmt, ##__VA_ARGS__)
+#else
+#define warn(...)
+#endif
+
+#if MESSAGE_LEVEL >= 2
+#define info(fmt, ...) printf("I-" fmt, ##__VA_ARGS__)
 #else
 #define info(...)
 #endif
 
-#if MESSAGE_LEVEL >= 1
-#define debug(fmt, ...) printf("D-" fmt, __VA_ARGS__)
+#if MESSAGE_LEVEL >= 3
+#define debug(fmt, ...) printf("D-" fmt, ##__VA_ARGS__)
 #else
 #define debug(...)
 #endif
