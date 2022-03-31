8170e28f7d7f177003925b28e8cee8fecfe720be
lichen-liu <issac.liu@mail.utoronto.ca>
2022-03-31 12:24:24 -0400

    combine utils into header only

--- src/tp/utils.h
+++ src/tp/utils.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <thread>
+#include <sstream>
 
 namespace TP
 {
-    const char *bool_to_cstr(bool val);
+    inline const char *bool_to_cstr(bool val)
+    {
+        return val ? "true" : "false";
+    }
 
-    std::string to_string(std::thread::id id);
+    inline std::string to_string(std::thread::id id)
+    {
+        std::stringstream ss;
+        ss << id;
+        return ss.str();
+    }
 }
