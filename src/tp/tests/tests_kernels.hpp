28a703d4356e0b01f857ea15a8ef1d96a1243017
lichen-liu <issac.liu@mail.utoronto.ca>
2022-04-04 11:29:45 -0400

    refactor collatz_conjecture_tests

--- src/tp/tests/tests_kernels.hpp
+++ src/tp/tests/tests_kernels.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdio>
+#include <functional>
+#include <memory>
 #include <vector>
 
 #include "task.hpp"
@@ -19,30 +21,64 @@ namespace TESTS
         return tasks;
     }
 
-    inline size_t collatz_conjecture_kernel(size_t lower, size_t upper)
+    inline size_t collatz_conjecture_kernel(size_t lower, size_t upper, size_t num_attempts = 1)
     {
         size_t step = 0;
-        for (size_t i = lower; i < upper; i++)
+        for (size_t itry = 0; itry < num_attempts; itry++)
         {
-            if (i == 0)
+            size_t local_step = 0;
+            for (size_t i = lower; i < upper; i++)
             {
-                continue;
-            }
-            size_t num = i;
-            while (num != 1)
-            {
-                if (num % 2 == 0)
+                if (i == 0)
                 {
-                    num /= 2;
+                    continue;
                 }
-                else
+                size_t num = i;
+                while (num != 1)
                 {
-                    num *= 3;
-                    num++;
+                    if (num % 2 == 0)
+                    {
+                        num /= 2;
+                    }
+                    else
+                    {
+                        num *= 3;
+                        num++;
+                    }
+                    step++;
                 }
-                step++;
             }
+            step += local_step;
         }
-        return step;
+
+        return step / num_attempts;
+    }
+
+    inline std::tuple<std::function<size_t()>, std::vector<TP::RAW_TASK>, std::unique_ptr<std::atomic<size_t>>>
+    generate_collatz_conjecture_tasks()
+    {
+        constexpr size_t num_attempts = 1;
+        constexpr size_t offset = 0;
+        constexpr size_t num_shards = 50000;
+        constexpr size_t shard_size = 200;
+
+        auto result_ptr = std::make_unique<std::atomic<size_t>>(0);
+        std::vector<TP::RAW_TASK> tasks;
+        for (int i = 0; i < num_shards; i++)
+        {
+            auto modified_kernel = [i, result = result_ptr.get()]
+            {
+                *result += TESTS::collatz_conjecture_kernel(offset + i * shard_size, offset + (i + 1) * shard_size, num_attempts);
+                // printf("step %d done\n", i);
+            };
+            tasks.emplace_back(modified_kernel);
+        }
+
+        auto single_task = []()
+        {
+            return collatz_conjecture_kernel(offset, offset + num_shards * shard_size);
+        };
+
+        return {std::move(single_task), std::move(tasks), std::move(result_ptr)};
     }
 }
--- src/tp/tests/wspdr_collatz_conjecture_tests.cc
+++ src/tp/tests/wspdr_collatz_conjecture_tests.cc
@@ -15,45 +15,32 @@ UTST_TEST(computation)
 {
     TIMER timer("computation");
 
-    constexpr int num_shards = 1000000;
-    constexpr int shard_size = 10;
-    std::atomic<size_t> result = 0;
-    std::vector<RAW_TASK> tasks;
-    for (int i = 0; i < num_shards; i++)
-    {
-        auto modified_kernel = [i, &result]
-        {
-            result += TESTS::collatz_conjecture_kernel(i * shard_size, (i + 1) * shard_size);
-            // printf("step %d done\n", i);
-        };
-        tasks.emplace_back(modified_kernel);
-    }
-
+    auto [serial_task, tasks, result_ptr] = TESTS::generate_collatz_conjecture_tasks();
     timer.elapsed_previous("init");
 
     // Serial execution result
-    size_t serial_result = TESTS::collatz_conjecture_kernel(0, num_shards * shard_size);
-    printf("serial total_num_steps=%lu for num_shards=%d shard_size=%d\n", serial_result, num_shards, shard_size);
+    size_t serial_result = serial_task();
+    printf("serial total_num_steps=%lu\n", serial_result);
     timer.elapsed_previous("serial");
 
     // Serial chunk execution result
-    result = 0;
+    *result_ptr = 0;
     for (const auto &task : tasks)
     {
         task();
     }
-    size_t serial_chunk_result = result;
-    printf("serial chunk total_num_steps=%lu for num_shards=%d shard_size=%d\n", serial_chunk_result, num_shards, shard_size);
+    size_t serial_chunk_result = *result_ptr;
+    printf("serial chunk total_num_steps=%lu\n", serial_chunk_result);
     timer.elapsed_previous("serial_chunk");
 
     // Pool execution result
-    result = 0;
+    *result_ptr = 0;
     WSPDR pool(4);
     pool.start();
     timer.elapsed_previous("pool_init");
     pool.execute(tasks);
-    size_t pool_result = result;
-    printf("pool total_num_steps=%lu for num_shards=%d shard_size=%d\n", pool_result, num_shards, shard_size);
+    size_t pool_result = *result_ptr;
+    printf("pool total_num_steps=%lu\n", pool_result);
     timer.elapsed_previous("pool");
 
     pool.status();
