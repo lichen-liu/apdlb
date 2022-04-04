acba28a2bb58259294e1bcd61c036d3f60ca0095
lichen-liu <issac.liu@mail.utoronto.ca>
2022-04-04 01:55:45 -0400

    add suap_simple_print_tests

---
+++ src/tp/tests/suap_simple_print_tests.cc
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cstdio>
+
+#include "tests_kernels.hpp"
+#include "utst.hpp"
+#include "suap_pool.hpp"
+
+using namespace TP;
+
+UTST_MAIN();
+
+namespace
+{
+    std::unique_ptr<SUAP> quick_launch(size_t num_workers, const std::vector<RAW_TASK> &tasks)
+    {
+        auto pool = std::make_unique<SUAP>(num_workers);
+        pool->start();
+        pool->execute(tasks);
+        return pool;
+    }
+}
+
+UTST_TEST(simple)
+{
+    quick_launch(2, TESTS::generate_simple_print_tasks(2));
+}
+
+UTST_TEST(simple_with_idle_worker)
+{
+    quick_launch(4, TESTS::generate_simple_print_tasks(2));
+}
+
+UTST_TEST(multi_session)
+{
+    constexpr int size = 32;
+    SUAP pool(size);
+    pool.start();
+    for (int i = 1; i <= size; i++)
+    {
+        printf("generate_simple_print_tasks(%d)\n", i);
+        pool.execute(TESTS::generate_simple_print_tasks(i));
+        pool.status();
+    }
+}
