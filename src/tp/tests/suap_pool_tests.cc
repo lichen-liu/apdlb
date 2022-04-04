d6f80689a59f43f9098572342efd07fcc047b3a9
lichen-liu <issac.liu@mail.utoronto.ca>
2022-04-04 13:50:27 -0400

    test clean up

--- src/tp/tests/suap_pool_tests.cc
+++ src/tp/tests/suap_pool_tests.cc
@@ -1,19 +1,54 @@
-#define MESSAGE_LEVEL 1
+#define MESSAGE_LEVEL 0
 
+#include <algorithm>
 #include <cstdio>
 
 #include "tests_kernels.hpp"
 #include "timer.hpp"
 #include "utst.hpp"
-#include "wspdr_pool.hpp"
+#include "suap_pool.hpp"
 
 using namespace TP;
 
 UTST_MAIN();
 
-UTST_TEST(computation)
+namespace
 {
-    TIMER timer("computation");
+    std::unique_ptr<SUAP_POOL> quick_launch(size_t num_workers, const std::vector<RAW_TASK> &tasks)
+    {
+        auto pool = std::make_unique<SUAP_POOL>(num_workers);
+        pool->start();
+        pool->execute(tasks);
+        return pool;
+    }
+}
+
+UTST_TEST(simple)
+{
+    quick_launch(2, TESTS::generate_simple_print_tasks(2));
+}
+
+UTST_TEST(simple_with_idle_worker)
+{
+    quick_launch(4, TESTS::generate_simple_print_tasks(2));
+}
+
+UTST_TEST(multi_session)
+{
+    constexpr int size = 32;
+    SUAP_POOL pool(size);
+    pool.start();
+    for (int i = 1; i <= size; i++)
+    {
+        printf("generate_simple_print_tasks(%d)\n", i);
+        pool.execute(TESTS::generate_simple_print_tasks(i));
+        pool.status();
+    }
+}
+
+UTST_TEST(collatz_conjecture)
+{
+    TIMER timer("collatz_conjecture");
 
     auto [serial_task, tasks, result_ptr] = TESTS::generate_collatz_conjecture_tasks();
     timer.elapsed_previous("init");
@@ -35,7 +70,7 @@ UTST_TEST(computation)
 
     // Pool execution result
     *result_ptr = 0;
-    WSPDR_POOL pool(4);
+    SUAP_POOL pool(4);
     pool.start();
     timer.elapsed_previous("pool_init");
     pool.execute(tasks);
