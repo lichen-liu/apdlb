c54bde5d86eddc619c7cb52c79c908ff1d20b1c1
lichen-liu <issac.liu@mail.utoronto.ca>
2022-04-06 22:00:53 -0400

    update matvecp_bm

--- benchmark/kernels/matvecp_bm.cpp
+++ benchmark/kernels/matvecp_bm.cpp
@@ -1,10 +1,5 @@
-#include <algorithm>
-#include <vector>
-//  int ms_rand(int& seed)
-// {
-//   seed = seed*0x343fd+0x269EC3;  // a=214013, b=2531011
-//   return (seed >> 0x10) & 0x7FFF;
-// }
+#include <cstdlib>
+
 void matvecp_kernel(size_t lower, size_t upper)
 {
     const size_t offset = 1;
@@ -14,32 +9,60 @@ void matvecp_kernel(size_t lower, size_t upper)
     {
         const size_t n = offset + i * scale;
 
-        auto gen = []()
+        // Input mat
+        float **mat = new float *[n];
+        for (size_t ii = 0; ii < n; ii++)
         {
-            float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-            return r;
-        };
-
-        // Input
-        std::vector<std::vector<float>> mat(n, std::vector<float>(n, 0));
-        for (auto &row_vec : mat)
-        {
-            std::generate(row_vec.begin(), row_vec.end(), gen);
+            mat[ii] = new float[n];
         }
-        std::vector<float> vec(n);
-        std::generate(vec.begin(), vec.end(), gen);
+        // Generate random data
+        int seed = static_cast<int>(i);
+        for (size_t ii = 0; ii < n; ii++)
+        {
+            for (size_t jj = 0; jj < n; jj++)
+            {
+                seed = seed * 0x343fd + 0x269EC3; // a=214013, b=2531011
+                float rand_val = (seed >> 0x10) & 0x7FFF;
+                mat[ii][jj] = static_cast<float>(rand_val) / static_cast<float>(RAND_MAX);
+            }
+        }
+
+        // Input vec
+        float *vec = new float[n];
+        for (size_t ii = 0; ii < n; ii++)
+        {
+            seed = seed * 0x343fd + 0x269EC3; // a=214013, b=2531011
+            float rand_val = (seed >> 0x10) & 0x7FFF;
+            vec[ii] = static_cast<float>(rand_val) / static_cast<float>(RAND_MAX);
+        }
 
         // Output
-        std::vector<float> res(n, 0);
+        float *res = new float[n];
 
         // Computation
         for (size_t row_idx = 0; row_idx < n; row_idx++)
         {
+            float result = 0;
             for (size_t col_idx = 0; col_idx < n; col_idx++)
             {
-                res[row_idx] += mat[row_idx][col_idx] * vec[col_idx];
+                result += mat[row_idx][col_idx] * vec[col_idx];
             }
+            res[row_idx] = result;
         }
+
+        // Clean up
+        // Output
+        delete[] res;
+
+        // Input vec
+        delete[] vec;
+
+        // Input mat
+        for (size_t ii = 0; ii < n; ii++)
+        {
+            delete[] mat[ii];
+        }
+        delete[] mat;
     }
 }
 
