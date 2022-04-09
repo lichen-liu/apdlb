160e0c2dcc8409c6d57334175c8f83ad75ecef58
Lichen Liu <issac.liu@mail.utoronto.ca>
2022-04-09 03:43:21 -0400

    add test.cpp for quick debugging

---
+++ test.cpp
@@ -0,0 +1,22 @@
+int foo()
+{
+    int arr_raw[100];
+    int *arr = arr_raw;
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = 0;
+    }
+}
+int bar()
+{
+    int arr_raw[100];
+    int *arr = arr_raw;
+
+    for (int i = 0; i < 10; i++)
+    {
+        auto task = [=]()
+        {
+            arr[i] = 0;
+        };
+    }
+}
