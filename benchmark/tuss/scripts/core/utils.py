c6782371e3e04d2091e58d0d0727ccde19c3b423
Lichen Liu <issac.liu@mail.utoronto.ca>
2022-04-16 01:23:09 -0400

    remove benchmark/tuss

--- benchmark/tuss/scripts/core/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def get_pf_timestamp_str():
-    '''
-    Get a pathname friendly timestamp string.
-    Down to seconds
-    '''
-    return datetime.now().strftime("%Y%m%d_%H%M%S")
