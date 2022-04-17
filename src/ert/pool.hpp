85e9938bd4b12cb955ae4851d8efeb6fa1f5ac43
Lichen Liu <issac.liu@mail.utoronto.ca>
2022-04-16 23:39:34 -0400

    minor pool.hpp

--- src/ert/pool.hpp
+++ src/ert/pool.hpp
@@ -10,11 +10,11 @@ namespace ERT
         explicit POOL(size_t num_workers) : num_workers_(num_workers) {}
         virtual ~POOL() = default;
 
-        virtual void start(){};
-        virtual void terminate(){};
+        virtual void start() {}
+        virtual void terminate() {}
         // A single session of execution, blocking until completed
         virtual void execute(const std::vector<RAW_TASK> &tasks) = 0;
-        virtual void status() const {};
+        virtual void status() const {}
 
         size_t num_workers() const { return this->num_workers_; }
 
